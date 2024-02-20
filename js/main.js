241a533f5f7a43e7b4c19ef3a637bcaf422a35cf
unknown <jihadissa610@gmail.com>
2024-02-20 22:09:27 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// FAQ Accordion
+document.addEventListener('DOMContentLoaded', () => {
+    const faqContainer = document.querySelector('.faq-content');
+
+    faqContainer.addEventListener('click', (e) => {
+        const groupHeader = e.target.closest('.faq-group-header');
+        if(!groupHeader) return;
+        const group = groupHeader.parentElement;
+        const groupBody= group.querySelector('.faq-group-body');
+        const icon = groupHeader.querySelector('i');
+        //toggle icon
+        icon.classList.toggle('fa-plus');
+        icon.classList.toggle('fa-minus');
+        //toggle visibility of body
+        groupBody.classList.toggle('open');
+        //close other opened FAQ bodies
+        const otherGroups = faqContainer.querySelectorAll('.faq-group');
+        otherGroups.forEach((otherGroup) => {
+            if(otherGroup !== group){
+                const otherGroupBody = otherGroup.querySelector('.faq-group-body');
+                const otherIcon = otherGroup.querySelector('.faq-group-header i');
+
+                otherGroupBody.classList.remove('open');
+                otherIcon.classList.remove('fa-minus');
+                otherIcon.classList.add('fa-plus');
+            }
+        })
+    })
+});
+
+//Mobile Menu
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerButton = document.querySelector('.hamburger-button');
+    const MobileMenu = document.querySelector('.mobile-menu');
+
+    hamburgerButton.addEventListener('click', () => {
+        MobileMenu.classList.toggle('active')
+    })
+})
